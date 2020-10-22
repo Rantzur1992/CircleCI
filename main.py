4dc8dd86e23af15384e37f144f9d9a1690cc94ac
rantz <ran.tzur@testproject.io>
2020-10-22 15:35:11 +0300

    V2

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from src.testproject.sdk.drivers import webdriver
 from src.testproject.sdk.drivers.webdriver.chrome import ChromeOptions
+from selenium.webdriver.common.by import By
 
 
 def simple_test():
-    platforms = ['WINDOWS', 'MAC', 'LINUX']
-    for platform in platforms:
-        chrome_options = ChromeOptions()
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-        driver.get("http://localhost:3000")
-        a = driver.find_element_by_class_name("App-header").text
-        passed = "Test Automation" in a
-        print("Test passed") if passed else print("Test failed")
-        driver.quit()
+    chrome_options = ChromeOptions()
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get("http://localhost:3000")
+    a = driver.find_element(By.XPATH, "//*[@id='root']/div/div/div[2]/div/code").text
+    passed = "Test Automation" in a
+    print("Test passed") if passed else print("Test failed")
+    driver.quit()
 
 
 if __name__ == "__main__":
